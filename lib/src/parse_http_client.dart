68ffd1e10608ba123628b587b696162a8055537c
Alann Maulana <alann_it06@yahoo.co.id>
2019-09-24 23:51:27 +0700

    update http client to comply external client

--- lib/src/parse_http_client.dart
+++ lib/src/parse_http_client.dart
@@ -10,22 +10,52 @@ import 'parse_user.dart';
 final ParseHTTPClient parseHTTPClient = ParseHTTPClient._internal();
 
 class ParseHTTPClient {
-  ParseHTTPClient._internal() : this._httpClient = parseBaseHTTPClient();
+  ParseHTTPClient._internal()
+      : this._httpClient =
+            parse.configuration.httpClient ?? _ParseBaseHTTPClient();
 
-  final parseBaseHTTPClient _httpClient;
+  final http.BaseClient _httpClient;
 
   String _getFullUrl(String path) {
     return parse.configuration.uri.origin + path;
   }
 
-  Future<dynamic> parseResponse(http.Response httpResponse) {
+  Future<Map<String, String>> _addHeader(
+      Map<String, String> additionalHeaders) async {
+    assert(parse.applicationId != null);
+    final headers = additionalHeaders ?? <String, String>{};
+
+    headers["User-Agent"] = "Dart Parse SDK v${kParseSdkVersion}";
+    headers['X-Parse-Application-Id'] = parse.applicationId;
+
+    // client key can be null with self-hosted Parse Server
+    if (parse.clientKey != null) {
+      headers['X-Parse-Client-Key'] = parse.clientKey;
+    }
+
+    headers['X-Parse-Client-Version'] = "dart${kParseSdkVersion}";
+
+    final currentUser = await ParseUser.currentUser;
+    if (currentUser != null && currentUser.sessionId != null) {
+      headers['X-Parse-Session-Token'] = currentUser.sessionId;
+    }
+
+    return headers;
+  }
+
+  Future<dynamic> _parseResponse(http.Response httpResponse,
+      {bool ignoreResult = false}) {
     String response = httpResponse.body;
     final result = json.decode(response);
 
     if (parse.enableLogging) {
       print("╭-- JSON");
-      _httpClient.printWrapped(response);
+      _parseLogWrapped(response);
       print("╰-- result");
+    }
+
+    if (ignoreResult) {
+      return null;
     }
 
     if (result is Map<String, dynamic>) {
@@ -44,123 +74,118 @@ class ParseHTTPClient {
         code: ParseException.invalidJson, message: 'invalid server response');
   }
 
-  Future<dynamic> get(String path,
-      {Map<String, dynamic> params, Map<String, String> headers}) {
+  Future<dynamic> get(
+    String path, {
+    Map<String, dynamic> params,
+    Map<String, String> headers,
+  }) async {
+    headers = await _addHeader(headers);
     final url = _getFullUrl(path);
 
     if (params != null) {
-      try {
-        final uri = Uri.parse(url).replace(queryParameters: params);
-        return _httpClient
-            .get(uri, headers: headers)
-            .then((r) => parseResponse(r));
-      } on FormatException catch (e) {
-        throw ParseException.fromThrow(e);
-      }
+      final uri = Uri.parse(url).replace(queryParameters: params);
+      return _httpClient
+          .get(uri, headers: headers)
+          .then((r) => _parseResponse(r));
     }
 
-    return _httpClient.get(url, headers: headers).then((r) => parseResponse(r));
+    return _httpClient
+        .get(url, headers: headers)
+        .then((r) => _parseResponse(r));
   }
 
-  Future<dynamic> delete(String path,
-      {Map<String, String> params, Map<String, String> headers}) {
+  Future<dynamic> delete(
+    String path, {
+    Map<String, String> params,
+    Map<String, String> headers,
+  }) async {
+    headers = await _addHeader(headers);
     final url = _getFullUrl(path);
 
     if (params != null) {
-      try {
-        var uri = Uri.parse(url).replace(queryParameters: params);
-        return _httpClient.delete(uri, headers: headers).then((r) {
-          return parseResponse(r);
-        });
-      } on FormatException catch (e) {
-        throw ParseException.fromThrow(e);
-      }
+      var uri = Uri.parse(url).replace(queryParameters: params);
+      return _httpClient.delete(uri, headers: headers).then((r) {
+        return _parseResponse(r);
+      });
     }
 
     return _httpClient
         .delete(url, headers: headers)
-        .then((r) => parseResponse(r));
+        .then((r) => _parseResponse(r));
   }
 
-  Future<dynamic> post(String path,
-      {Map<String, String> headers, dynamic body, Encoding encoding}) {
+  Future<dynamic> post(
+    String path, {
+    Map<String, String> headers,
+    dynamic body,
+    Encoding encoding,
+    bool ignoreResult = false,
+  }) async {
+    headers = await _addHeader(headers);
     final url = _getFullUrl(path);
 
     return _httpClient
         .post(url, headers: headers, body: body, encoding: encoding)
-        .then((r) => parseResponse(r));
+        .then((r) => _parseResponse(r, ignoreResult: ignoreResult));
   }
 
-  Future<dynamic> put(String path,
-      {Map<String, String> headers, dynamic body, Encoding encoding}) {
+  Future<dynamic> put(
+    String path, {
+    Map<String, String> headers,
+    dynamic body,
+    Encoding encoding,
+  }) async {
+    headers = await _addHeader(headers);
     final url = _getFullUrl(path);
 
     return _httpClient
         .put(url, headers: headers, body: body, encoding: encoding)
-        .then((r) => parseResponse(r));
+        .then((r) => _parseResponse(r));
   }
 }
 
-class parseBaseHTTPClient extends http.BaseClient {
+class _ParseBaseHTTPClient extends http.BaseClient {
   final http.Client _client;
 
-  parseBaseHTTPClient()
-      : this._client = parse.configuration.httpClient ?? http.Client();
+  _ParseBaseHTTPClient() : this._client = http.Client();
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
-    assert(parse.applicationId != null);
-
-    request.headers["user-agent"] = "Dart Parse SDK v${kParseSdkVersion}";
-    request.headers['X-Parse-Application-Id'] = parse.applicationId;
-
-    // client key can be null with self-hosted Parse Server
-    if (parse.clientKey != null) {
-      request.headers['X-Parse-Client-Key'] = parse.clientKey;
-    }
-
-    request.headers['X-Parse-Client-Version'] = "dart${kParseSdkVersion}";
-
-    final currentUser = await ParseUser.currentUser;
-    if (currentUser != null && currentUser.sessionId != null) {
-      request.headers['X-Parse-Session-Token'] = currentUser.sessionId;
-    }
-
     if (parse.enableLogging) {
-      _logging(request);
+      logToCURL(request);
     }
 
     return await _client.send(request);
   }
+}
 
-  void _logging(http.BaseRequest request) {
-    var curlCmd = "curl -X ${request.method} \\\n";
-    var compressed = false;
-    var bodyAsText = false;
-    request.headers.forEach((name, value) {
-      if (name.toLowerCase() == "accept-encoding" &&
-          value.toLowerCase() == "gzip") {
-        compressed = true;
-      } else if (name.toLowerCase() == "content-type") {
-        bodyAsText =
-            value.contains('application/json') || value.contains('text/plain');
-      }
-      curlCmd += ' -H "$name: $value" \\\n';
-    });
-    if (<String>['POST', 'PUT', 'PATCH'].contains(request.method)) {
-      if (request is http.Request) {
-        curlCmd +=
-            " -d '${bodyAsText ? request.body : request.bodyBytes}' \\\n  ";
-      }
+void logToCURL(http.BaseRequest request) {
+  var curlCmd = "curl -X ${request.method} \\\n";
+  var compressed = false;
+  var bodyAsText = false;
+  request.headers.forEach((name, value) {
+    if (name.toLowerCase() == "accept-encoding" &&
+        value.toLowerCase() == "gzip") {
+      compressed = true;
+    } else if (name.toLowerCase() == "content-type") {
+      bodyAsText =
+          value.contains('application/json') || value.contains('text/plain');
     }
-    curlCmd += (compressed ? " --compressed " : " ") + request.url.toString();
-    print("╭-- cURL");
-    printWrapped(curlCmd);
-    print("╰-- (copy and paste the above line to a terminal)");
+    curlCmd += ' -H "$name: $value" \\\n';
+  });
+  if (<String>['POST', 'PUT', 'PATCH'].contains(request.method)) {
+    if (request is http.Request) {
+      curlCmd +=
+          " -d '${bodyAsText ? request.body : request.bodyBytes}' \\\n  ";
+    }
   }
+  curlCmd += (compressed ? " --compressed " : " ") + request.url.toString();
+  print("╭-- cURL");
+  _parseLogWrapped(curlCmd);
+  print("╰-- (copy and paste the above line to a terminal)");
+}
 
-  void printWrapped(String text) {
-    final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
-    pattern.allMatches(text).forEach((match) => print(match.group(0)));
-  }
+void _parseLogWrapped(String text) {
+  final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+  pattern.allMatches(text).forEach((match) => print(match.group(0)));
 }
