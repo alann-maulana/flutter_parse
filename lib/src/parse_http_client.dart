be5fb4a996ce68408d6824f579b99614277e0a1c
Alann Maulana <alann_it06@yahoo.co.id>
2020-08-24 19:06:21 +0700

    print console without split on web

--- lib/src/parse_http_client.dart
+++ lib/src/parse_http_client.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_parse/src/config/config.dart';
 import 'package:http/http.dart' as http;
 
@@ -195,6 +196,10 @@ void logToCURL(http.BaseRequest request) {
 }
 
 void _parseLogWrapped(String text) {
+  if (kIsWeb) {
+    print(text);
+    return;
+  }
   final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
   pattern.allMatches(text).forEach((match) => print(match.group(0)));
 }
